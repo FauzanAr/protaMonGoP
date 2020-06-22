a8d97e2f22d319f6eff514b4f1c5c170d17550be
FauzanAr <faurinta12345@gmail.com>
2020-06-22 13:43:02 +0700

    adding passport to app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDb = require('./config/db')
 
 // Load config 
 dotenv.config({
     path: './config/config.env'
 })
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDb()
 
@@ -23,6 +29,20 @@ app.engine('.hbs', exphbs({
     defaultLayout: 'main', 
     extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+// Express session
+app.use(session({
+    secret: 'pak polisi',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
